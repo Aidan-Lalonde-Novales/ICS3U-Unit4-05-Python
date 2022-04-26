8d11b95c91611dae074cceaa6942094a2d716062
Aidan-Lalonde-Novales <ubuntu@ip-172-31-55-218.ec2.internal>
2022-04-26 04:47:06 +0000

    lll

---
+++ positive_adder.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+# Created by Aidan Lalonde-Novales
+# Created April 2022
+# This program adds a desired amount of numbers together with user input.
+
+
+def main():
+    # this function asks for the number of times to add numbers
+    # then asks for what numbers in specific to add.
+    times_added = 0
+    total_sum = 0
+
+    # input
+    add_loop_string = input("Enter how many numbers you want to add: ")
+
+    # additional input & process
+    try:
+
+        add_loop = int(add_loop_string)
+
+        for times_added in range(add_loop):
+            number_to_add_string = input("Enter a number: ")
+            number_to_add = int(number_to_add_string)
+
+            if number_to_add < 0:
+                continue
+
+            total_sum = total_sum + number_to_add
+            times_added += 1
+
+        # output
+        print("\nThe sum of your numbers is {0}.".format(total_sum))
+
+    except Exception:
+        print("\nYour integer is invalid. Please re-run.")
+
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
